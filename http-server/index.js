29f8c2a54764dedf4e8c7acbb6a44ef9d8ff68ea
PSreeVarun <162110212+PSreeVarun@users.noreply.github.com>
2024-10-10 17:06:52 +0530

    Update index.js

--- http-server/index.js
+++ http-server/index.js
@@ -1,28 +1,7 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
 const minimist = require('minimist');
-
-const app = express();
 const argv = minimist(process.argv.slice(2));
 const PORT = argv.port || 3000;  // Default to port 3000 if not provided
 
-// Serve the home page (if applicable)
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'home.html'));
-});
-
-// Serve the projects page
-app.get('/project.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'project.html'));
-});
-
-// Serve the registration page
-app.get('/registration', (req, res) => {
-    res.sendFile(path.join(__dirname, 'registration.html'));
-});
-
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
